caa60df89850e691dc0d91d2d3d80cc7c73b6cdd
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-07-29 09:50:23 +0100

    fix: add schema

---
+++ model/Post.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const PostSChema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "A Blog Post must have a title"],
+    },
+    description: {
+      type: String,
+      required: [true, "A Blog Post must have a description"],
+    },
+    tags: [String],
+    readCount: {
+      type: Number,
+      default: 0,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    state: {
+      type: String,
+      enum: ["draft", "published"],
+      default: "draft",
+    },
+    body: {
+      type: String,
+      required: [true, "A Blog Post must contain a body"],
+    },
+    readTime: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", PostSChema);
+
+export default Post;
