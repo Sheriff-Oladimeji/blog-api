52646e5a8a58d220db0556213e63467c0fb4b1c2
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-01 07:39:23 +0100

    fix: update post controller

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,17 +1,16 @@
 import Post from "../model/Post.model";
 import User from "../model/User.model";
 const getAllPublishedPost = async (req, res) => {
-    try {
-        const posts = await Post.find({ state: "Published" })
-         res.status(200).json({
-           status: "success",
-           posts,
-         });
-        
-    } catch (error) {
-          throw err;
-    }
-}
+  try {
+    const posts = await Post.find({ state: "Published" });
+    res.status(200).json({
+      status: "success",
+      posts,
+    });
+  } catch (error) {
+    throw err;
+  }
+};
 
 const getSinglePublishedPost = async (req, res) => {
   try {
@@ -40,42 +39,42 @@ const getSinglePublishedPost = async (req, res) => {
 };
 
 const createAPost = async (req, res) => {
-    try {
-        const { title, description, tags, body } = req.body;
+  try {
+    const { title, description, tags, body } = req.body;
 
-        //calculate read time of post from the body passed in
-        const wpm = 225; //wpm => word per minute
-        const numberOfWords = body.trim().split(/\s+/).length;
-        const readTime = Math.ceil(numberOfWords / wpm);
+    //calculate read time of post from the body passed in
+    const wpm = 225; //wpm => word per minute
+    const numberOfWords = body.trim().split(/\s+/).length;
+    const readTime = Math.ceil(numberOfWords / wpm);
 
-        //get author name and author Id
-        let { firstname, lastname } = req.user;
-        let author = `${firstname} ${lastname}`;
-        let authorId = req.user._id;
-        const post = await Post.create({
-            title,
-            description,
-            tags,
-            body,
-            author,
-            authorId,
-            readTime,
-        });
+    //get author name and author Id
+    let { firstname, lastname } = req.user;
+    let author = `${firstname} ${lastname}`;
+    let authorId = req.user._id;
+    const post = await Post.create({
+      title,
+      description,
+      tags,
+      body,
+      author,
+      authorId,
+      readTime,
+    });
 
-        //add the new created post to 'posts' array property on the user document
-        let user = await User.findById(req.user._id);
-        user.posts.push(post._id);
-        await user.save(); //save changes made to the user doc
+    //add the new created post to 'posts' array property on the user document
+    let user = await User.findById(req.user._id);
+    user.posts.push(post._id);
+    await user.save(); //save changes made to the user doc
 
-        //send back response
-        res.status(201).json({
-            status: "success",
-            post,
-        });
-    } catch (err) {
-        throw err
-    }
-}
+    //send back response
+    res.status(201).json({
+      status: "success",
+      post,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
 
 const updateAPost = async (req, res) => {
   const { state, body } = req.body;
@@ -108,24 +107,25 @@ const deleteAPost = async (req, res) => {
     const post = await Post.findByIdAndRemove(req.params.postId, {
       authorId: req.user.id,
     });
-    if(!post) return res.status(404).json({
-      status: 'Fail',
-      message: 'Post with given Id not found'
-    })
-    
+    if (!post)
+      return res.status(404).json({
+        status: "Fail",
+        message: "Post with given Id not found",
+      });
+
     if (post.authorId.toString() !== req.user.id) {
       return res.status(401).json({
         status: "Fail",
         message: `You can only delete a post you created!`,
       });
     }
-    
-//delete post from 'posts' array in user the document
+
+    //delete post from 'posts' array in user the document
     const postByUser = await User.findById(req.user._id);
     postByUser.posts.pull(post._id);
     await postByUser.updateOne({ posts: postByUser.posts });
-    
-//return deleted post
+
+    //return deleted post
     res.status(200).json({
       status: "success",
       message: "Post deleted successfully",
@@ -133,4 +133,12 @@ const deleteAPost = async (req, res) => {
   } catch (err) {
     throw err;
   }
+};
+
+export {
+  getAllPublishedPost,
+  getSinglePublishedPost,
+  createAPost,
+  deleteAPost,
+  updateAPost,
 };
